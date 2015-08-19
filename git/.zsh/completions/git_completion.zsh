5b2bc66ee5b0de57442726776537f3fb610e8b37
Tobias Nett <skaldarnar@googlemail.com>
2015-08-19 23:37:03 +0200

    Revised `git` configurations.

--- git/.zsh/completions/git_completion.zsh
+++ git/.zsh/completions/git_completion.zsh
@@ -1,6 +1,11 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where it is
 # located in an ArchLinux installation, at least).
+
+# ArchLinux
 completion='/usr/share/git/completion/git-completion.zsh'
+
+# Homebrew
+#completioin='$(brew --prefix)/share/zsh/site-functions/_git'
 
 [[ -f $completion ]] && fpath=( $completion $fpath )
