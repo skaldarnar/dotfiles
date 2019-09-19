19b732c1a281f846907de6652a38740cdf4036d5
Tobias Nett <skaldarnar@googlemail.com>
2019-09-19 14:29:44 +0200

    update zsh config

--- zsh/.zsh/antigen.zsh
+++ zsh/.zsh/antigen.zsh
@@ -1,8 +1,8 @@
 # Load Antigen as plug-in manager for ZSH
 #   https://github.com/zsh-users/antigen
 
-ANTIGEN_ZSH="$ZSH/antigen/antigen.zsh"
-#ANTIGEN_ZSH="/usr/local/share/antigen/antigen.zsh"  # system-wide antigen installation via brew
+#ANTIGEN_ZSH="$ZSH/antigen/antigen.zsh"
+ANTIGEN_ZSH="/usr/local/share/antigen/antigen.zsh"  # system-wide antigen installation via brew
 
 if [[ -a $ANTIGEN_ZSH ]]; then
   source $ANTIGEN_ZSH
@@ -17,17 +17,17 @@ if [[ -a $ANTIGEN_ZSH ]]; then
   antigen bundle git-extras
   #antigen bundle github
   antigen bundle gradle
-  antigen bundle mvn
-  antigen bundle pj             # remember to set `$PROJECT_PATHS`
+  #antigen bundle mvn
+  #antigen bundle pj             # remember to set `$PROJECT_PATHS`
   antigen bundle rbenv
   antigen bundle ruby
   antigen bundle sbt
   antigen bundle scala
-  #antigen bundle ssh-agent
+  antigen bundle ssh-agent
   antigen bundle sudo
-  antigen bundle systemd
+  #antigen bundle systemd
 
-  antigen bundle zsh-users/zsh-completions src
+  #antigen bundle zsh-users/zsh-completions src
   antigen bundle zsh-users/zsh-syntax-highlighting
 
   # Load the theme
